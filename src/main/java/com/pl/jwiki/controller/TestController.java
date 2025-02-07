239df144a00def9df019433b22222428ff2fb4dd
laopengge <2131654667@qq.com>
2025-02-07 16:22:39 +0800

    2.7 增加hello world post接口，测试test.http

--- src/main/java/com/pl/jwiki/controller/TestController.java
+++ src/main/java/com/pl/jwiki/controller/TestController.java
@@ -1,8 +1,8 @@
 package com.pl.jwiki.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +10,11 @@ public class TestController {
     //http://127.0.0.1:8880/hello
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! POST," + name;
     }
 }
