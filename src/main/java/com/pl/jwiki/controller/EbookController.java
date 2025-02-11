b10bc552f9d0220bdda94046fcaf4b278546bc82
laopengge <2131654667@qq.com>
2025-02-11 16:26:13 +0800

    3.8 为了统一后端接口增加了CommonResp通用类

--- src/main/java/com/pl/jwiki/controller/EbookController.java
+++ src/main/java/com/pl/jwiki/controller/EbookController.java
@@ -2,22 +2,28 @@ package com.pl.jwiki.controller;
 
 
 import com.pl.jwiki.domain.Ebook;
+import com.pl.jwiki.resp.CommonResp;
 import com.pl.jwiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController  //RestController返回字符串和json  Controller返回的是jsp,html页面  RestController=Controller+ResponseBody
+@RequestMapping("/ebook")
 public class EbookController {
 
     @Resource
     private EbookService ebookService;
 
-    @GetMapping("/ebook/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    @GetMapping("/list")
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
