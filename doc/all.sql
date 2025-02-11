d06a4c2f8b8adec70d42d7f15585ff74bda5951a
laopengge <2131654667@qq.com>
2025-02-11 14:14:14 +0800

    3.3 将数据库改为jwikidev

--- doc/all.sql
+++ doc/all.sql
@@ -6,4 +6,8 @@ create table test1 (
     primary key (id)
 )engine=innodb default charset =utf8mb4 comment '测试';
 
+insert into test1 (id, name, password)
+values (1, 'java', '2134');
+
+
 select * from  test1;
