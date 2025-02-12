16053ed2166b292085c5a80e87104829f611e58d
laopengge <2131654667@qq.com>
2025-02-12 21:09:41 +0800

    4.3 网站首页布局开发

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+// 使用Ant Design of Vue 4.x版本开发时，报错无法解析antd样式文件。
+// 查看依赖文件发现并不存在antd.css文件，查阅文档得知，这是1.7.8版本的使用方式，在最新的4.x版本中已经将antd.css文件修改名称为reset.css文件
+import 'ant-design-vue/dist/reset.css'
 
 // @ts-ignore
 createApp(App).use(store).use(router).use(Antd).mount('#app')
