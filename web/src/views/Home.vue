7b32f77ab92e08e55bcdc8e23421593ffed0d235
laopengge <2131654667@qq.com>
2025-02-13 15:40:09 +0800

    5.2 解决跨域问题

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -55,8 +55,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'; // @ is an alias to /src
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+  setup() {
+    console.log("setup");
+    axios.get("http://localhost:8880/ebook/list?name=spring").then(function (response) {
+      console.log(response);
+    });
+  }
 });
 </script>
