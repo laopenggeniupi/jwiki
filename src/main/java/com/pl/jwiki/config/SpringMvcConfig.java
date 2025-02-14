ca282b65e25dd053a9126876eb1f979a0e492b18
laopengge <2131654667@qq.com>
2025-02-14 19:15:06 +0800

    5.15 使用aop拦截请求，打印接口耗时，需要添加aop依赖

--- src/main/java/com/pl/jwiki/config/SpringMvcConfig.java
+++ src/main/java/com/pl/jwiki/config/SpringMvcConfig.java
@@ -1,23 +1,23 @@
-package com.pl.jwiki.config;
-
-import com.pl.jwiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-/**
- * 使用拦截器还要配置一个SpringMvcConfig类处理拦截请求
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+// package com.pl.jwiki.config;
+//
+// import com.pl.jwiki.interceptor.LogInterceptor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// import javax.annotation.Resource;
+//
+// /**
+//  * 使用拦截器还要配置一个SpringMvcConfig类处理拦截请求
+//  */
+// @Configuration
+// public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//     @Resource
+//     LogInterceptor logInterceptor;
+//
+//     public void addInterceptors(InterceptorRegistry registry) {
+//         registry.addInterceptor(logInterceptor)
+//                 .addPathPatterns("/**").excludePathPatterns("/login");
+//     }
+// }
